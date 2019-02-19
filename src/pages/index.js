c25ca2b38ff75ed5179b0405ab1b458ad0b89277
Manindra Gautam <manindra@akaruilabs.com>
2019-02-19 21:59:37 +0530

    tailwind setup completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,7 @@
 import React from 'react'
 
-export default () => <div>Hello world!</div>
+export default () => <div class='flex justify-center bg-grey-lighter'>
+  <div class='text-grey-darker text-center bg-grey-light px-4 py-2 m-2'>.</div>
+  <div class='text-grey-darker text-center bg-grey-light px-4 py-2 m-2'>Hello World</div>
+  <div class='text-grey-darker text-center bg-grey-light px-4 py-2 m-2'>.</div>
+</div>
