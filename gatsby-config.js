7dc1ee74036729aefbefe3b89926189a3fb01687
Manindra Gautam <manindragautamofficial@gmail.com>
2019-02-24 14:02:44 +0530

    added: web manifest generator

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,31 @@ module.exports = {
                 path: `${__dirname}/content`,
                 name: `content`,
             },
-        }
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: "Manindra Gautam",
+                short_name: "MKGautam",
+                start_url: "/",
+                background_color: "#ffffff",
+                theme_color: "#ffffff",
+                // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+                // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+                display: "standalone",
+                icons: [
+                    {
+                        "src": `/favicons/icons-192.png`,
+                        "type": "image/png",
+                        "sizes": "192x192"
+                    },
+                    {
+                        "src": `/favicons/icons-512.png`,
+                        "type": "image/png",
+                        "sizes": "512x512"
+                    }
+                ]
+            },
+        },
     ]
 }
