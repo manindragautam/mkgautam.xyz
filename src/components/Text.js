9c800dee7f7e329938fc63578c973b39c19f3069
Manindra Gautam <manindragautamofficial@gmail.com>
2019-03-03 13:59:15 +0530

    added: site report and adjusted text alignment

--- src/components/Text.js
+++ src/components/Text.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default ({ children }) => (
-    <p className="text-grey-darker text-base lg:text-lg mt-6">
+    <p className="text-grey-darker text-base text-justify lg:text-lg mt-6">
         {children}
     </p>
 )
